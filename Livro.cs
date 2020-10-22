cb353f1d8592c5d35394286e3b98c09d1fa826e0
TulioliAles <desmecad@hotmail.com>
2020-10-21 21:15:06 -0300

    Mapeamento das entidades em dominio separado

---
+++ Livro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookStore.Domain
+{
+    public class Livro
+    {
+        public Livro()
+        {
+            this.Autores = new List<Autor>();
+        }
+
+        public int Id { get; set; }
+
+        public string Titulo { get; set; }
+
+        public string ISBN { get; set; }
+
+        public DateTime DataLancamento { get; set; }
+
+        public int CategoriaId { get; set; }
+
+        public Categoria Categoria { get; set; }
+
+        public ICollection<Autor> Autores { get; set; }
+    }
+}
